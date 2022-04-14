7df592b94f35551d294d73e3b7fc946d1efdbf8c
Lien Zhu <lienzhucs@gmail.com>
2022-04-14 13:25:23 -0500

    duplicate html and check not working

--- todo-script.js
+++ todo-script.js
@@ -5,8 +5,37 @@
 const apiKey = "96364a-276feb-952475-c85e9e-d6e333";
 
 let inputForm = document.getElementById("add-item-form");
+inputForm.addEventListener("submit", addItem);
 
-loadPage();
+loadItems();
+
+
+// add eventlisteners to checkbox and delete buttons
+//
+function addHandlers() {
+    let buttons = document.getElementsByClassName("delete-button-class");
+
+    for (i = 0; i < buttons.length; i++) {
+        let button = buttons[i];
+        button.addEventListener("click", deleteItem);
+    }
+
+    let checkboxes = document.getElementsByClassName("complete-checkbox-class");
+
+    for (j = 0; j < checkboxes.length; j++) {
+        let box = checkboxes[j];
+        box.addEventListener("click", setComplete);
+    }
+}
+
+
+
+// FROM DISCUSSION WITH PROF. CLAPP: 
+//
+// load items from server into array
+// pass array into drawPage()
+// add event listeners to each button in another method
+//
 
 
 
@@ -20,22 +49,91 @@ function clearPage() {
     for (i = 0; i < currItems.length; i++) {
         contentArea.removeChild(currItems[i]);
     }
+
+    console.log("page cleared");
 }
 
 
 
-// function that loads todo items into dom elements
+// DISPLAY ALL ITEMS
 //
-function loadPage() {
+function drawPage(itemsArr) {
 
-    console.log("start of loadPage()");
-
+    console.log("drawPage() called");
 
     // clear page to prevent duplicate display by calling clearPage()
     //
     clearPage();
-    
-    console.log("page cleared");
+
+    // iterate through each item in itemsArr
+    // create dom elements
+    // for each item in todos {
+    //      make some html elements with data attributes with value of the id
+    //      add to dom
+    //      if (complete == true) {
+    //          style with strikethrough
+    //      }
+    //  }
+     
+    for (i = 0; i < itemsArr.length; i++) {
+
+        let item = itemsArr[i];
+
+        // print debug
+        //
+        console.log(item.text + " is being displayed");
+        console.log(item);
+
+        let ItemDiv = document.createElement("div");
+        ItemDiv.className = "todo-item-wrapper";
+        ItemDiv.setAttribute("data-id", item.id);
+
+        let ItemText = document.createElement("p");
+        ItemText.className = "item-text-class";
+        ItemText.innerHTML = item.text;
+
+        // strikethrough complete items
+        //
+        if (item.completed == true) {
+            ItemText.style.textDecoration = "line-through";
+        } else {
+            ItemText.style.textDecoration = "none";
+        }
+
+        let ItemBox = document.createElement("input");
+        ItemBox.type = "checkbox";
+        ItemBox.className = "complete-checkbox-class";
+        ItemBox.setAttribute("data-id", item.id);
+
+        let ItemDelBtn = document.createElement("input");
+        ItemDelBtn.type = "button";
+        ItemDelBtn.className = "delete-button-class";
+        ItemDelBtn.value = "delete";
+        ItemDelBtn.setAttribute("data-id", item.id);
+
+        // do this later
+        //
+        // ItemDelBtn.addEventListener("click", deleteItem);
+
+        let contentArea = document.getElementById("main-wrapper");
+        contentArea.appendChild(ItemDiv);
+        
+        ItemDiv.appendChild(ItemBox);
+        ItemDiv.appendChild(ItemText);
+        ItemDiv.appendChild(ItemDelBtn);
+    }
+
+    console.log("items drawn");
+
+    addHandlers();
+}
+
+
+// LOAD ITEMS FROM SERVER
+//
+function loadItems() {
+
+    console.log("start of loadItems()");
     
     var loadX = new XMLHttpRequest();
 
@@ -44,62 +142,13 @@ function loadPage() {
 
             var todos = JSON.parse(this.responseText);
             
-            // for each item in todos {
-            //      make some html elements with data attributes with value of the id
-            //      add to dom
-            //      if (complete == true) {
-            //          style with strikethrough
-            //      }
-            //  }
-            
-            for (i = 0; i < todos.length; i++) {
 
-                let item = todos[i];
-
-                // print debug
-                //
-                console.log(item.text + " is being displayed");
-
-                let ItemDiv = document.createElement("div");
-                ItemDiv.className = "todo-item-wrapper";
-                ItemDiv.setAttribute("data-id", item.id);
-
-                let ItemText = document.createElement("p");
-                ItemText.className = "item-text-class";
-                ItemText.innerHTML = item.text;
-
-                // strikethrough complete items
-                //
-                if (item.complete == true) {
-                    ItemText.style.textDecoration = "line-through";
-                } else {
-                    ItemText.style.textDecoration = "none";
-                }
-
-                let ItemBox = document.createElement("input");
-                ItemBox.type = "checkbox";
-                ItemBox.className = "complete-checkbox-class";
-                ItemBox.setAttribute("data-id", item.id);
-
-                let ItemDelBtn = document.createElement("input");
-                ItemDelBtn.type = "button";
-                ItemDelBtn.className = "delete-button-class";
-                ItemDelBtn.value = "delete";
-                ItemDelBtn.setAttribute("data-id", item.id);
-                ItemDelBtn.addEventListener("click", deleteItem);
-
-                let contentArea = document.getElementById("main-wrapper");
-                contentArea.appendChild(ItemDiv);
-                
-                ItemDiv.appendChild(ItemBox);
-                ItemDiv.appendChild(ItemText);
-                ItemDiv.appendChild(ItemDelBtn);
-            }
+            drawPage(todos);
 
 
             // print debug
             //
-            console.log("end of loadPage()");
+            console.log("end of loadItems()");
         }
     };
 
@@ -110,7 +159,7 @@ function loadPage() {
 
 
 
-// add a todo item
+// ADD TODO ITEM
 //
 function addItem(event) {
     event.preventDefault();
@@ -134,9 +183,9 @@ function addItem(event) {
             // parse JSON response
             var todo = JSON.parse(this.responseText);
 
-            // after adding item, call loadPage()
+            // after adding item, call loadItems()
             // 
-            loadPage();
+            loadItems();
 
             // print debug
             //
@@ -159,20 +208,63 @@ function addItem(event) {
     document.getElementById("add-input").value = "";
 }
 
-inputForm.addEventListener("submit", addItem);
 
 
 
-// set complete status on checkbox click
+
+// SET COMPLETE STATUS ON CHECKBOX CLICK
 //
 function setComplete() {
+    let id = this.getAttribute("data-id");
 
+    if (this.checked) {
+        var data = {
+            "completed": true
+        }
+    } else {
+        var data = {
+            "completed": false
+        }
+    }
+    
+
+    // Initalize AJAX Request
+    var checkX = new XMLHttpRequest();
+
+    // Response handler
+    checkX.onreadystatechange = function() {
+
+        // Wait for readyState = 4 & 200 response
+        if (this.readyState == 4 && this.status == 200) {
+
+            // parse JSON response
+            var todo = JSON.parse(this.responseText);
+
+            // after updating item, call loadItems()
+            // 
+            loadItems();
+
+            // print debug
+            //
+            console.log("a todo was added: " + todo.text);
+
+        } else if (this.readyState == 4) {
+            // this.status !== 200, error from server
+            console.log(this.responseText);
+        }
+    }
+
+    checkX.open("PUT", "https://cse204.work/todos/"+id, true);
+
+    checkX.setRequestHeader("Content-type", "application/json");
+    checkX.setRequestHeader("x-api-key", apiKey);
+    checkX.send(JSON.stringify(data));
 }
 
 
 
 
-// request to delete an item
+// DELETE ITEM
 //
 function deleteItem() {
     // Setting variable for form input (get from HTML form)
@@ -186,8 +278,10 @@ function deleteItem() {
 
         // Wait for readyState = 4 & 200 response
         if (this.readyState == 4 && this.status == 200) {
-
-            loadPage();
+            
+            // after adding, load page
+            //
+            loadItems();
 
             // print debug
             //
@@ -206,8 +300,6 @@ function deleteItem() {
     delX.setRequestHeader("Content-type", "application/json");
     delX.setRequestHeader("x-api-key", "96364a-276feb-952475-c85e9e-d6e333");
     delX.send();
-
-    
 }
 
 
